c9ad96cab6d272b1ab3a58e44564a907a268a75b
Devin <devinsullins@gmail.com>
2020-12-21 19:13:10 -0600

    studio 1 final

--- mission-planning-dashboard/src/app/app.module.ts
+++ mission-planning-dashboard/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { CrewComponent } from './component/crew/crew.component';
+import { EquipmentComponent } from './component/equipment/equipment.component';
+import { ExperimentsComponent } from './component/experiments/experiments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CrewComponent,
+    EquipmentComponent,
+    ExperimentsComponent
   ],
   imports: [
     BrowserModule
